ab3b3fd4df951dd115491ef64d7b0c60c6d0dae4
Noah Snow <realmrmagicpie@gmail.com>
2020-11-28 15:58:36 -0500

    Importing Code

---
+++ aliases.sh
@@ -0,0 +1,41 @@
+
+# LS Commands
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -a -CF'
+#alias l='ls -CF'
+
+# Misc
+
+alias py='python3'
+alias p='python3'
+
+alias mkdir='mkdir -p'
+
+# CD Alternatives
+
+alias adir='cd /etc/apache2/sites-available'
+alias vdir='cd /var/www'
+alias udir='cd /etc/ufw'
+alias aqua='cd /var/www/AquaHQ'
+alias isthicc='cd /var/www/IsThicc.xyz'
+alias bois='cd /var/www/BoisBot'
+
+# Random
+
+alias no='echo yes'
+alias yes='echo no'
+
+# Lmao
+
+alias o='sh /root/root-config/scripts/o.sh'
+
+# Random Echos
+
+alias thicc='''echo """
+😏
+"""'''
+alias doge='''echo """
+doge is omega cute
+"""'''
